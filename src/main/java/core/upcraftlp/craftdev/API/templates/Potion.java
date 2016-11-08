b12e55a6914e1db55c421daff6466484c868e3f2
Unknown <j-schirra@gmx.de>
2016-11-08 14:23:21 +0100

    v0.2.1

--- src/main/java/core/upcraftlp/craftdev/API/templates/Potion.java
+++ src/main/java/core/upcraftlp/craftdev/API/templates/Potion.java
@@ -1,7 +1,5 @@
 package core.upcraftlp.craftdev.API.templates;
 
-import net.minecraft.entity.EntityLivingBase;
-
 public class Potion extends net.minecraft.potion.Potion {
 
 	public Potion(String name, boolean isBadEffectIn, int liquidColorIn) {
@@ -9,9 +7,4 @@ public class Potion extends net.minecraft.potion.Potion {
 		this.setRegistryName(name);
 		this.setPotionName(name);
 	}
-	
-	@Override
-	public void performEffect(EntityLivingBase entityLivingBaseIn, int p_76394_2_){
-	}
-
 }
--- src/main/java/core/upcraftlp/craftdev/common/main/CraftDevReference.java
+++ src/main/java/core/upcraftlp/craftdev/common/main/CraftDevReference.java
@@ -8,7 +8,7 @@ public class CraftDevReference {
 
 	public static final String MCVERSIONS = "1.10.2,1.9.4";
 
-	public static final String VERSION = "0.2.0-b4";
+	public static final String VERSION = "0.2.1";
 	public static final String MODNAME = "CraftDev Core";
 	public static final String MODID = "craftdev-core";
 	public static final String UPDATE_JSON = "https://raw.githubusercontent.com/UpcraftLP/CraftDev-Core/master/Version.json";
