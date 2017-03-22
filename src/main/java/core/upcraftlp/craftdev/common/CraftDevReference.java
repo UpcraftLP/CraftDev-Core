0801b19521c67282dad9fdeafcab98fa7dd94969
Unknown <j-schirra@gmx.de>
2017-03-22 21:24:12 +0100

    removed the TextFormatting import from the reference class

--- src/main/java/core/upcraftlp/craftdev/common/CraftDevReference.java
+++ src/main/java/core/upcraftlp/craftdev/common/CraftDevReference.java
@@ -2,8 +2,6 @@ package core.upcraftlp.craftdev.common;
 
 import java.time.Year;
 
-import net.minecraft.util.text.TextFormatting;
-
 public class CraftDevReference {
 
     public static final String MCVERSIONS = "[1.11]";
@@ -13,9 +11,10 @@ public class CraftDevReference {
     public static final String MODID = "craftdevcore";
     public static final String UPDATE_JSON = "https://raw.githubusercontent.com/UpcraftLP/CraftDev-Core/master/Version.json";
     public static final String UPDATE_URL = "https://minecraft.curseforge.com/projects/craftdev-core";
-    public static final String CREDITS = TextFormatting.GOLD + "\u00A9" + Year.now().getValue() + " UpcraftLP";
+    
+    public static final String CREDITS = "\u00A9" + Year.now().getValue() + " UpcraftLP";
     public static final String[] authors = new String[] { "UpcraftLP" };
-    public static final String MOD_DESCRIPTION = TextFormatting.AQUA + "core mod for Upcraft's mods";
+    public static final String MOD_DESCRIPTION = "core mod for Upcraft's mods";
     
     private static final String BASE_PACKAGE = "core.upcraftlp.craftdev";
     public static final String GUI_FACTORY = BASE_PACKAGE + ".config.CoreConfigGuiFactory";
